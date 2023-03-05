bf62672f47e46487e61af29dea10d27a9d4cd0e6
Putinjov <putinjov@gmail.com>
2023-03-05 19:12:40 +0200

    final

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup)
+
+galleryContainer.addEventListener('click', showModal);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>
+    `;
+    })
+    .join("");
+
+}
+
+function showModal(evt) {
+    evt.preventDefault()
+    const bigImage = evt.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src="${bigImage}" width="800" height="600">
+`)
+
+    instance.show() 
+    
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup)
+const lightbox = new SimpleLightbox('.gallery a', { caption: true, captionsData: 'alt', captionDelay: 250 });
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+    <li><a class="gallery__item" href="${original}">
+        <img style="display: block" class="gallery__image" src="${preview}" alt="${description}" />
+    </a></li>
+    `;
+    })
+    .join("");
+    
+}
+
+
+
